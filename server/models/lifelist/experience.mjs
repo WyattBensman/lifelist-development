f32d2e2313334b33bf669a1f0a3439463634d3df
WyattBensman <wyattbensman5@gmail.com>
2024-02-14 17:42:01 -0500

    Completed Experience Schema

--- server/models/lifelist/experience.mjs
+++ server/models/lifelist/experience.mjs
@@ -1,8 +1,55 @@
 import { Schema, model } from "mongoose";
 
-const experienceSchema = new new Schema({})();
+const experienceSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  coordinates: {
+    type: {
+      latitude: {
+        type: Number,
+        required: true,
+      },
+      longitude: {
+        type: Number,
+        required: true,
+      },
+    },
+    required: true,
+  },
+  types: {
+    type: [String],
+    enum: [
+      "Attractions",
+      "Destinations",
+      "Events",
+      "Courses",
+      "Venues",
+      "Festivals",
+      "Hikes & Trails",
+      "Resorts",
+      "Concerts",
+      "Artists",
+    ],
+    required: true,
+  },
+  collages: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Collage",
+    },
+  ],
+});
 
-// Create Model
 const Experience = model("Experience", experienceSchema);
 
 export default Experience;
