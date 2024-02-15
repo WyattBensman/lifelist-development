8f1432b0a84964b501ca2f7b9d09f539467daa1f
WyattBensman <wyattbensman5@gmail.com>
2024-02-15 07:30:53 -0500

    Added file structure to resolvers, completed file structure for queries

---
+++ server/schemas/resolvers/queries/index.mjs
@@ -0,0 +1,6 @@
+import * as userQueries from "./user";
+import * as cameraQueries from "./camera";
+import * as collageQueries from "./collage";
+import * as experienceQueries from "./experience";
+
+export { userQueries, cameraQueries, collageQueries, experienceQueries };
