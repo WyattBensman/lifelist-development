0e4978054cec5600530f6e401e469c750b1afcc7
WyattBensman <wyattbensman5@gmail.com>
2024-02-15 08:22:57 -0500

    Added initial file structure to Mutation Resolvers

---
+++ server/schemas/resolvers/mutations/index.mjs
@@ -0,0 +1,19 @@
+import * as userMutations from "./user";
+import * as userRelationsMutations from "./userRelations";
+import * as collageCreationMutations from "./collageCreation";
+import * as collageActionsMutations from "./collageActions";
+import * as cameraMutations from "./camera";
+import * as lifeListMutations from "./lifeList";
+import * as notificationsMutations from "./notifications";
+import * as messageMutations from "./message";
+
+export {
+  userMutations,
+  userRelationsMutations,
+  collageCreationMutations,
+  collageActionsMutations,
+  cameraMutations,
+  lifeListMutations,
+  notificationsMutations,
+  messageMutations,
+};
