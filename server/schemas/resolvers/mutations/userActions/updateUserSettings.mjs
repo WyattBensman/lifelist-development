7c00d6ec6072c6f62cef5923e9899f0d0fa71eb9
WyattBensman <wyattbensman5@gmail.com>
2024-02-16 12:04:51 -0500

    Minor touch up

---
+++ server/schemas/resolvers/mutations/userActions/updateUserSettings.mjs
@@ -0,0 +1,31 @@
+import { isCurrentUser } from "../../../../utils/auth.mjs";
+
+export const updateUserSettings = async (
+  _,
+  { userId, privacy, darkMode, language, notifications },
+  { user }
+) => {
+  try {
+    // Authentication
+    isCurrentUser(user, userId);
+
+    // Update the user's settings
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      {
+        settings: {
+          privacy,
+          darkMode,
+          language,
+          notifications,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+
+    return updatedUser;
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    throw new Error("An error occurred during user settings update.");
+  }
+};
