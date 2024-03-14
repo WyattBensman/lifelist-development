df43071961cd14d8ab01c082940b6d5b4b34f8a3
WyattBensman <wyattbensman5@gmail.com>
2024-03-14 09:31:20 -0400

    FINISHED MESSAGING QUERIES

--- server/schemas/resolvers/queries/messaging/messagingQueries.mjs
+++ server/schemas/resolvers/queries/messaging/messagingQueries.mjs
@@ -3,16 +3,7 @@ import { isUser } from "../../../../utils/auth.mjs";
 
 export const getUserConversations = async (_, __, { user }) => {
   try {
-    /* isUser(user); */
-    const user = {
-      _id: "65e72e4e82f12a087695250d",
-      conversations: [
-        {
-          conversation: "65e8e27fef629d9763169403",
-          isRead: true,
-        },
-      ],
-    };
+    isUser(user);
 
     // Get conversation IDs from the user's conversations field
     const conversationIds = user.conversations.map(
@@ -40,7 +31,7 @@ export const getUserConversations = async (_, __, { user }) => {
 
 export const getConversation = async (_, { conversationId }, { user }) => {
   try {
-    /* isUser(user); */
+    isUser(user);
 
     const conversation = await Conversation.findById(conversationId)
       .populate({
@@ -71,9 +62,9 @@ export const getUnreadMessagesCount = async (_, __, { user }) => {
   try {
     isUser(user);
 
-    const userWithUnreadCount = await User.findById(
-      "65e72e4e82f12a087695250d"
-    ).select("unreadMessagesCount");
+    const userWithUnreadCount = await User.findById(user._id).select(
+      "unreadMessagesCount"
+    );
 
     if (!userWithUnreadCount) {
       throw new Error("User not found for the provided ID.");
