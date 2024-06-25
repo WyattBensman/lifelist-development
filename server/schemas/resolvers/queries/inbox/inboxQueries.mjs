817d1b6cef51aa06855a2f1dbd2fbf100448f5e0
WyattBensman <wyattbensman5@gmail.com>
2024-06-25 09:58:44 -0400

    rebuild

--- server/schemas/resolvers/queries/inbox/inboxQueries.mjs
+++ server/schemas/resolvers/queries/inbox/inboxQueries.mjs
@@ -5,54 +5,40 @@ import { isUser } from "../../../../utils/auth.mjs";
 export const getUserConversations = async (_, __ /* { user } */) => {
   /* isUser(user); */
 
-  try {
-    console.log("Fetching user with ID: 663a3129e0ffbeff092b81d4");
+  // Find the authenticated user
+  const currentUser = await User.findById("663a3129e0ffbeff092b81d4").populate(
+    "conversations._id"
+  );
 
-    // Find the authenticated user
-    const currentUser = await User.findById(
-      "663a3129e0ffbeff092b81d4"
-    ).populate("conversations.conversation");
+  if (!currentUser) {
+    throw new Error("User not found");
+  }
 
-    if (!currentUser) {
-      console.log("User not found");
-      throw new Error("User not found");
-    }
+  // Extract the conversation IDs
+  const conversationIds = currentUser.conversations.map(
+    (conversation) => conversation._id
+  );
 
-    console.log("User found:", currentUser);
-
-    // Extract the conversation IDs
-    const conversationIds = currentUser.conversations.map(
-      (conversation) => conversation.conversation._id
-    );
-
-    console.log("Conversation IDs:", conversationIds);
-
-    // Find and populate the conversations
-    const foundConversations = await Conversation.find({
-      _id: { $in: conversationIds },
-    })
-      .populate({
-        path: "lastMessage",
-        populate: {
-          path: "sender",
-          model: "User",
-          select: "_id username fullName profilePicture",
-        },
-      })
-      .populate({
-        path: "participants",
+  // Find and populate the conversations
+  const foundConversations = await Conversation.find({
+    _id: { $in: conversationIds },
+  })
+    .populate({
+      path: "lastMessage",
+      populate: {
+        path: "sender",
         model: "User",
         select: "_id username fullName profilePicture",
-      })
-      .exec();
+      },
+    })
+    .populate({
+      path: "participants",
+      model: "User",
+      select: "_id username fullName profilePicture",
+    })
+    .exec();
 
-    console.log("Found conversations:", foundConversations);
-
-    return foundConversations;
-  } catch (error) {
-    console.error(`Error fetching conversations: ${error.message}`);
-    throw new Error("Failed to fetch conversations");
-  }
+  return foundConversations;
 };
 
 export const getConversation = async (_, { conversationId }, { user }) => {
