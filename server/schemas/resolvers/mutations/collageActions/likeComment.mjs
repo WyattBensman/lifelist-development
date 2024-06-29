3b0eb420078f624557f255f7dcfbe94ea756b028
WyattBensman <wyattbensman5@gmail.com>
2024-06-28 23:21:17 -0400

    minor corrections

--- server/schemas/resolvers/mutations/collageActions/likeComment.mjs
+++ server/schemas/resolvers/mutations/collageActions/likeComment.mjs
@@ -1,16 +1,16 @@
 import { Comment } from "../../../../models/index.mjs";
 import { isUser } from "../../../../utils/auth.mjs";
 
-const likeComment = async (_, { commentId } /* { user } */) => {
+const likeComment = async (_, { commentId }, { user }) => {
   try {
-    /* isUser(user); */
+    isUser(user);
 
-    const comment = await Comment.findById("667011518fc375d3be82f4c1");
+    const comment = await Comment.findById(commentId);
     if (!comment) {
       throw new Error("Comment not found");
     }
 
-    await comment.like("663a3129e0ffbeff092b81d4");
+    await comment.like(user._id);
 
     return {
       success: true,
--- server/schemas/resolvers/mutations/collageActions/unlikeComment.mjs
+++ server/schemas/resolvers/mutations/collageActions/unlikeComment.mjs
@@ -1,16 +1,16 @@
 import { Comment } from "../../../../models/index.mjs";
 import { isUser } from "../../../../utils/auth.mjs";
 
-const unlikeComment = async (_, { commentId } /* { user } */) => {
+const unlikeComment = async (_, { commentId }, { user }) => {
   try {
-    /* isUser(user); */
+    isUser(user);
 
-    const comment = await Comment.findById("667011518fc375d3be82f4c1");
+    const comment = await Comment.findById(commentId);
     if (!comment) {
       throw new Error("Comment not found");
     }
 
-    await comment.unlike("663a3129e0ffbeff092b81d4");
+    await comment.unlike(user._id);
 
     return {
       success: true,
