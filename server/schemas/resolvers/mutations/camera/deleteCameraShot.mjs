c8f235988ec4232497c4446035b8e884fbf35e19
WyattBensman <wyattbensman5@gmail.com>
2024-07-25 08:02:04 -0400

    Corrected DeleteCameraShot mutation - need future changes

--- server/schemas/resolvers/mutations/camera/deleteCameraShot.mjs
+++ server/schemas/resolvers/mutations/camera/deleteCameraShot.mjs
@@ -7,35 +7,32 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const deleteCameraShot = async (_, { shotId }, { user }) => {
   try {
-    /* isUser(user); */
+    isUser(user);
 
     // Retrieve the camera shot from the database
     const shot = await CameraShot.findById(shotId);
     if (!shot) {
       return { success: false, message: "Camera shot not found." };
     }
-    console.log("Found Shot");
 
     // Ensure the user is authorized to delete this shot
-    /* if (shot.author.toString() !== user._id.toString()) {
+    if (shot.author.toString() !== user._id.toString()) {
       return {
         success: false,
         message: "User not authorized to delete this camera shot.",
       };
-    } */
+    }
 
     // Remove the shot ID from any albums it may be part of
     await CameraAlbum.updateMany(
       { shots: shotId },
       { $pull: { shots: shotId } }
     );
-    console.log("Removed from Camera Albums");
 
     // Remove the shot ID from the user's cameraShots field
-    await User.findByIdAndUpdate("663a3129e0ffbeff092b81d4", {
+    await User.findByIdAndUpdate(user._id, {
       $pull: { cameraShots: shotId },
     });
-    console.log("Removed from User");
 
     // Delete the image file from the filesystem
     /*     const filePath = path.join(__dirname, shot.image);
