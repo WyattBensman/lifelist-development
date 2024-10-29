ada8dabfcf769fdb248f39c9d2e21e1ce411a563
WyattBensman <wyattbensman5@gmail.com>
2024-10-28 20:59:57 -0400

    .

--- server/schemas/typeDefs.mjs
+++ server/schemas/typeDefs.mjs
@@ -332,8 +332,8 @@ type Score {
     # User Queries
     getUserProfileById(userId: ID!): UserProfileResponse
     getUserCounts(userId: ID!): UserCountsResponse
-    getFollowers(userId: ID!, limit: Int, lastSeenId: ID): [UserRelationsResponse]
-    getFollowing(userId: ID!, limit: Int, lastSeenId: ID): [UserRelationsResponse]
+    getFollowers(userId: ID!, limit: Int, offset: Int): [User]
+    getFollowing(userId: ID!, limit: Int, offset: Int): [User]
     getUserCollages(userId: ID!): [Collage]
     getRepostedCollages(userId: ID!): [Collage]
     getTaggedCollages(userId: ID!): [Collage]
@@ -418,8 +418,7 @@ type Score {
     fullName: String
     profilePicture: String
     settings: UserSettings
-    followRequests: [User]
-    lastCursor: ID
+    followRequests: [ID]
   }
 
   type CollageResponse {
